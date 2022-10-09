f8a95c21d7077a10662a1fd2a051b2b2575bf29c
wehsu01 <hsuw@spu.edu>
2022-10-08 19:31:26 -0700

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,39 @@
+# test
+# test 2
+print('hello wowrld')
+
+#input output
+example = input('eat ')
+print('i want', example,'!')
+
+#multiple variables
+fruit = 'apple'
+count = '3'
+print ('i want', count, fruit, '!')
+print ('i want ' + count, '' + fruit + '!')
+print (f'i want {fruit}!')
+print (f'i want {fruit.upper()}!')
+print ('i want {}!' .format(fruit))
+
+number_of_fruits1 = 5
+sum_of_two_fruits = 3 + 10
+
+number_of_fruits2 = str(5)
+
+print (type(number_of_fruits1))
+print()
+print (type(number_of_fruits2))
+
+# Integer, float, boolean, string
+test = 192.32
+testint = int(test)
+testfloat = (float(test))
+testbool = bool(test)
+teststr = str(test)
+print (test, type(test))
+print (testint, type(testint))
+print (testfloat, type (testfloat))
+print (testbool, type(testbool))
+print (teststr, type(teststr))
+
+#operators 
