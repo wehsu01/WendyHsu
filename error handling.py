e17ea1ee31b5da211fbddbf12b327b4baee06992
wehsu01 <hsuw@spu.edu>
2022-10-10 21:48:30 -0700

    10092022

---
+++ error handling.py
@@ -0,0 +1,11 @@
+#nums = ['1','2','3','4','5','6']
+
+#try: 
+    #print (sum(nums))
+
+#except: 
+#    print ('can not print')
+
+#finally: 
+#    print ('result')
+
---
+++ loop.py
@@ -0,0 +1,45 @@
+#nums = [1,2,3,4,5,6]
+
+#for num in nums:
+#    print (num + 1)
+
+#for i in range (10):
+#    print (i)
+
+#teams = [[1,2],['a','b'], ['!','@']]
+#for team in teams: 
+#    for name in team:
+#        print (name)
+
+#a = 0
+#while a < 11:
+#    print (a)
+#    a += 2
+
+#pass, break, continue
+
+
+#names = [0,1,2,3,4,5,6]
+
+#for name in names: 
+#    if not name:
+#        pass
+#    print (name)
+
+#      break #end loop
+#      pass #nothing gets executed when pass is in
+
+#a = [0, 1, 2]
+#for element in a:
+#     if not element:
+#         continue
+#     print(element)
+
+var = 10                  
+while var > 0:              
+   var = var -1
+   if var == 4:
+      break
+   print ('Current variable value :', var)
+print ("Good bye!")
+
